f50351d75e55a874ffe2c7c868f2bc1a207d5db8
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-12 17:22:21 -0500

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Use the reduce() function to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
+  return accumulator + currentBatch;
+}, 0);
+
+// Export totalBatteries if needed (for example, if you are testing with a separate file)
+module.exports = totalBatteries;
